a11df21cb6d30dae7f4eff2228fe587253acd468
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-01-23 17:05:45 +0700

    Update UI: add SearchTextField, SearchButton, WeatherImage

--- src/InsideSky.java
+++ src/InsideSky.java
@@ -1,10 +1,16 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class InsideSky extends JFrame {
     public InsideSky(){
         super("InsideSky");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        //getContentPane().setBackground(Color.BLACK);
         setSize(450, 650);
 
         // load the gui at the center of the screen
@@ -15,6 +21,40 @@ public class InsideSky extends JFrame {
 
         // prevent any resize of our gui
         setResizable(false);
+        addGuiComponents();
+    }
 
+    private void addGuiComponents(){
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(15, 15, 351, 45);
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        add(searchTextField);
+
+        JButton searchButton = new JButton();
+        searchButton.setBounds(375, 13, 47, 45);
+        searchButton.setIcon(loadImageIcon("src/assets/search.png", searchButton.getWidth(), searchButton.getHeight()));
+        searchButton.setContentAreaFilled(false);
+        searchButton.setBorderPainted(false);
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(searchButton);
+
+        JLabel weatherConditionImage = new JLabel();
+        weatherConditionImage.setBounds(50, 125, 300, 217);
+        weatherConditionImage.setIcon(loadImageIcon("src/assets/cloudyWX.png", weatherConditionImage.getWidth(), weatherConditionImage.getHeight()));
+        add(weatherConditionImage);
+    }
+
+    private ImageIcon loadImageIcon(String path, int width, int height)
+    {
+        try{
+            BufferedImage img = ImageIO.read(new File(path));
+
+            return new ImageIcon(img.getScaledInstance(width, height, Image.SCALE_SMOOTH));
+        }
+        catch(IOException e){
+           e.printStackTrace();
+    }
+        System.out.println("Could not find resource");
+        return null;
     }
 }
